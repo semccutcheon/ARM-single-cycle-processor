be24e15382da55b14e92149d5a5383330e4fcb74
Sean McCutcheon <Sean McCutcheon>
2017-03-02 20:59:32 -0800

    cfg_updates

--- verif/top.sv
+++ verif/top.sv
@@ -1,14 +1,20 @@
 module top(
-    input logic clk, reset,
-    output logic [31:0] WriteData, DataAdr,
-    output logic MemWrite);
+    input  logic clk, reset,
+    output logic [31:0] DataAdr,
+    output logic [31:0] WriteData,
+    output logic MemWrite
+    );
 
     logic [31:0] PC, Instr, ReadData;
 
+    logic [3:0] byteEnable;
+
 
     // instantiate processor and memories
-    arm arm(clk, reset, PC, Instr, MemWrite, DataAdr,
-        WriteData, ReadData);
-    imem imem(PC, Instr);
-    dmem dmem(clk, MemWrite, DataAdr, WriteData, ReadData);
+    arm  arm (clk, reset, PC, Instr, MemWrite, DataAdr, WriteData, ReadData, byteEnable);
+    
+    imem imem(PC, Instr); // not merged with RAM due not being able to load programs into RAM and needing tests.
+    
+    ram ram(clk, MemWrite, byteEnable, DataAdr, WriteData, ReadData);
+
 endmodule
